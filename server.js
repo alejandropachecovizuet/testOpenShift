cfc7fb883e5dffc8f7a18a5bca48881d2f14e9b2
ALEJANDRO PACHECO VIZUET <ALEJANDRO.PACHECO.VIZUET@GMAIL.COM>
2019-11-05 16:53:03 -0600

    os metrics 2

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 
 app.get('/', function (req, res) {
     let cpus=os.cpus();
+    let totalCpus=cpus.length;
     let memoryBytes=os.totalmem();
     let memoryGb=memoryBytes/1024/1024/1024;
-    res.send({app:'Hello World Openshift!!!', cpus, memoryBytes, memoryGb});
+    res.send({app:'Hello World Openshift!!!', cpus, memoryBytes, memoryGb,totalCpus});
 });
 
 app.listen(8080, function () {
