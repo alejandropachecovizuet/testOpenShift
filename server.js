424d3d6850b42b19d900fb78fbb45e6064af22ee
ALEJANDRO PACHECO VIZUET <ALEJANDRO.PACHECO.VIZUET@GMAIL.COM>
2019-11-05 16:37:38 -0600

    os metrics 1

--- server.js
+++ server.js
@@ -7,16 +7,9 @@ app.get('/', function (req, res) {
     let cpus=os.cpus();
     let memoryBytes=os.totalmem();
     let memoryGb=memoryBytes/1024/1024/1024;
-    let result={app:'Hello World Openshift!!!', cpus, memoryBytes, memoryGb}
-    
-/*    for (let index = 0; index < cpus.length; index++) {
-        const cpu = cpus[index];
-        str=str+'\n************'+JSON.stringify(cpu);
-        
-    }*/
-    res.send(result);
+    res.send({app:'Hello World Openshift!!!', cpus, memoryBytes, memoryGb});
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 8080!!!!!');
 });
